b3974a83463b670ac37a460b399d2cd8c99e60df
lz1irq <ivailo96@gmail.com>
2014-09-24 20:57:17 +0300

    Changed var names... I don't know why

--- 02_JavaStuff/src/Main.java
+++ 02_JavaStuff/src/Main.java
@@ -2,32 +2,32 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static Map<String, Integer> population = new HashMap<String, Integer>();
-	static int POPULATION_CRITERIA = 10000000;
+	static Map<String, Integer> countries = new HashMap<String, Integer>();
+	static int MIN_POPULATION = 10000000;
 	public static void main(String[] args) throws IOException {
 		loadCountries();
-		findCountries(POPULATION_CRITERIA);
+		findCountries(MIN_POPULATION);
 		capitalise();
 	}
 	
 	private static void loadCountries() {
-		population.put("Bulgaria", 7000000);
-		population.put("China", 1339190000);
-		population.put("Haiti", 10200000);
-		population.put("Bangladesh", 164000000);
-		population.put("Poland", 39000000);
-		population.put("Slovenia", 2000000);
-		population.put("Iraq", 32000000);
-		population.put("Monaco", 33000);
-		population.put("Switzerland", 8000000);
-		population.put("Nigeria", 170000000);
-		population.put("Croatia", 4000000);
-		population.put("Ukraine", 45000000);
+		countries.put("Bulgaria", 7000000);
+		countries.put("China", 1339190000);
+		countries.put("Haiti", 10200000);
+		countries.put("Bangladesh", 164000000);
+		countries.put("Poland", 39000000);
+		countries.put("Slovenia", 2000000);
+		countries.put("Iraq", 32000000);
+		countries.put("Monaco", 33000);
+		countries.put("Switzerland", 8000000);
+		countries.put("Nigeria", 170000000);
+		countries.put("Croatia", 4000000);
+		countries.put("Ukraine", 45000000);
 		
 	}
 	
 	private static void findCountries(int min_population) {
-		for(Map.Entry<String, Integer> country: population.entrySet()) {
+		for(Map.Entry<String, Integer> country: countries.entrySet()) {
 			if(country.getValue() > min_population) System.out.println(country.getKey());
 		}
 	}
