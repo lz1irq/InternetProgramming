33eae30b6b9df7b3d496901eb5e3e45f7776189e
lz1irq <lz1irq@tardis.medicom.bg>
2014-11-30 14:28:12 +0200

    homework 2 - selectors

---
+++ 04_SampleJQuery/WebContent/scripts/sampleJQuery2.js
@@ -0,0 +1,18 @@
+$(function() {
+	"use strict"
+	console.log($('#logo')); //id
+	console.log($('.sbutton')) //class
+	console.log($('#col1 a')); //all links which are children of div with id col1
+	console.log($('#col2 ul > li')); // all immediate children list items of unordered lists in #col2
+	console.log($('ul:first-child')); //the first child of every unordered list
+	console.log($('a[href*="elsys"]')); //all links pointing to elsys's domain
+	console.log($('#col2 ul > li:nth-child(odd)')); //only the odd elements of ...
+	console.log($('p:first-letter')); //the first letter of every paragraph
+	console.log($('ul li[rel="bookmark"]')); //all list items in an unordered list that have a specific property
+	console.log($('div > #latest')); //direct children of divs that have the 'latest' class
+	console.log($('input:not(input[type="email])')); // input elements that are not for email
+	console.log($('p a:only-child')); //links that are the only child of a paragraph
+	console.log($('h2+p')); //select the paragraphs preceded by a h2
+	console.log($('*')); //select all elements
+	console.log($('ul > li::before')); //just before every list item which is a direct child of an unordered list
+});
