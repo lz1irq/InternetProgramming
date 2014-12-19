94baa08b15e15573602eb96b9b0f220e355c2db8
lz1irq <ivaylo.markoff@gmail.com>
2014-12-19 20:10:05 +0200

    task 4

--- 05_JavaScript/hw.js
+++ 05_JavaScript/hw.js
@@ -1,10 +1,21 @@
 $(function() {
-	"use strict"
+	'use strict'
 	
 	//task 2
-	console.log($(".tu"));
-	//alternatively - "#footer > a", etc...
+	console.log($('.tu'));
+	//alternatively - '#footer > a', etc...
 	
 	//task 3
-	console.log($("#col1 > p").text());
+	console.log($('#col1 > p').text());
+
+	//task 4
+	var t4list = $('#menu-top-level-menu');
+	var t4li = $('<li/>');
+
+	var t4lia = $('<a/>');
+	t4lia.text('new button');
+	t4lia.attr('href', '#');
+	
+	t4li.append(t4lia);
+	t4list.append(t4li);
 });
