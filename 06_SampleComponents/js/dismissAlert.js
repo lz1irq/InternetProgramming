4a1f678cf9db6745d9bd822697d2619ab517dd60
lz1irq <ivaylo.markoff@gmail.com>
2015-02-19 18:47:48 +0200

    stop event propagation

--- 06_SampleComponents/js/dismissAlert.js
+++ 06_SampleComponents/js/dismissAlert.js
@@ -1,16 +1,19 @@
 $(document).ready(function() {
-	"use strict"
- // events are propagated from selected element
- // up to the document (through all parents)
- // we catch the event on the first parent - document
- $(document).on("click", "[data-dismiss-sample='alert']", function() {
- 	console.log("dismiss alert", arguments);
- });
-
- // all click events on the page
- // (except events that are not propagated)
- $(document).on("click", function() {
- 	console.log("all", arguments);
- });
-
+"use strict"
+// events are propagated from selected element
+// up to the document (through all parents)
+// we catch the event on the first parent - document
+$(document).on("click",
+// we filter only click events on elements
+// that have attribute data-dismiss-sample
+// with value 'alert'
+"[data-dismiss-sample='alert']", function(event) {
+event.stopPropagation();
+console.log("dismiss alert", arguments);
+});
+// all click events on the page
+// (except events that are not propagated)
+$(document).on("click", function() {
+console.log("all", arguments);
+});
 });
