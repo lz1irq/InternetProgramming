700c4ed1a912daf5714ec8b4fe0efefdb406eb63
lz1irq <ivaylo.markoff@gmail.com>
2015-02-19 18:49:55 +0200

    animation when removing alert

--- 06_SampleComponents/js/dismissAlert.js
+++ 06_SampleComponents/js/dismissAlert.js
@@ -8,6 +8,6 @@ $(document).on("click",
 // that have attribute data-dismiss-sample
 // with value 'alert'
 "[data-dismiss-sample='alert']", function(event) {
-$(event.target.closest(".alert")).remove();
+$(event.target.closest(".alert")).fadeOut("slow");
 });
 });
