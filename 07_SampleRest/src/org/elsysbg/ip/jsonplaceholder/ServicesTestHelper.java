57ba26a784c1885c9fb44fc4e43b79bfda54393e
lz1irq <ivaylo.markoff@gmail.com>
2015-03-12 17:13:39 +0200

    Fixed PostsRestTest to test only the PostsRest class by mocking the PostsService with mokkito

---
+++ 07_SampleRest/src/org/elsysbg/ip/jsonplaceholder/ServicesTestHelper.java
@@ -0,0 +1,9 @@
+package org.elsysbg.ip.jsonplaceholder;
+
+import org.elsysbg.ip.jsonplaceholder.service.PostsService;
+
+public class ServicesTestHelper {
+	public static void setPostsService(PostsService postsService) {
+		Services.setPostsService(postsService);
+	}
+}
