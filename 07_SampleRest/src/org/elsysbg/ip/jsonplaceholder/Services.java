acea73f350e8dcd93d495cb94891319d86dc47b1
lz1irq <ivaylo.markoff@gmail.com>
2015-03-19 19:44:55 +0200

    Working with Derby Embedded

--- 07_SampleRest/src/org/elsysbg/ip/jsonplaceholder/Services.java
+++ 07_SampleRest/src/org/elsysbg/ip/jsonplaceholder/Services.java
@@ -1,10 +1,14 @@
 package org.elsysbg.ip.jsonplaceholder;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.elsysbg.ip.jsonplaceholder.service.PostsService;
 
 public class Services {
 
 	private static PostsService postsService;
+	private static EntityManagerFactory entityManagerFactory;
 
 	// TODO synchronized should be done in a better way in real projects
 	public synchronized static PostsService getPostsService() {
@@ -19,4 +23,23 @@ public class Services {
 	static void setPostsService(PostsService postsService) {
 		Services.postsService = postsService;
 	}
+	
+	public synchronized static EntityManagerFactory getEntityManagerFactory() {
+				// lazy loading
+				if (entityManagerFactory == null) {
+					try {
+						Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+					} catch (ClassNotFoundException e) {
+						throw new IllegalStateException("No driver", e);
+					}
+					entityManagerFactory = Persistence.createEntityManagerFactory("07_SampleRest");
+				}
+				return entityManagerFactory;
+			}
+			
+			// for tests purposes
+			static void setEntityManagerFactory(EntityManagerFactory entityManagerFactory) {
+				Services.entityManagerFactory = entityManagerFactory;
+			}
+			
 }
--- 07_SampleRest/src/org/elsysbg/ip/jsonplaceholder/model/Post.java
+++ 07_SampleRest/src/org/elsysbg/ip/jsonplaceholder/model/Post.java
@@ -1,36 +1,51 @@
 package org.elsysbg.ip.jsonplaceholder.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
+// name specifies the name of the table that will hold this entity
+@Entity(name = "Posts")
+@NamedQueries({ @NamedQuery(name = "allPosts", query = "SELECT p from Posts p") })
 public class Post {
-	
+
+	@Id
+	@GeneratedValue
 	private long id;
-		private String title;
-		private String body;
-		
-		public long getId() {
-			return id;
-		}
-		
-		public void setId(long id) {
-			this.id = id;
-		}
-		
-		public String getTitle() {
-			return title;
-		}
-		
-		public void setTitle(String title) {
-			this.title = title;
-		}
-		
-		public String getBody() {
-			return body;
-		}
-		
-		public void setBody(String body) {
-			this.body = body;
-		}
 	
+	@Column(nullable=false, length=50)
+	private String title;
+	
+	@Column(nullable=false, length=500)
+	private String body;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
 }
